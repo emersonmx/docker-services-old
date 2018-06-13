84549cce790348c25a03ef82189b7ebb68436228
Emerson MX <emersonmx@gmail.com>
2018-06-13 11:15:41 -0300

    Update nginx configs

--- nginx/update-config.sh
+++ nginx/update-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo 'Updating configs...'
-docker cp ./nginx.conf docker-services_nginx_1:/etc/nginx/
-# docker cp conf.d/default.conf docker-services_php7_1:/etc/nginx/conf.d/
+docker cp nginx.conf docker-services_nginx_1:/etc/nginx/
+docker cp conf.d/default.conf docker-services_nginx_1:/etc/nginx/conf.d/
 echo 'Done.'
