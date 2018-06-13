8c2bb191bc12cf41c65f81c6cf0b5f07eb3b790d
Emerson MX <emersonmx@gmail.com>
2018-06-13 13:22:38 -0300

    Update script to update docker config

--- nginx/update-config.sh
+++ nginx/update-config.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+project_path="$(dirname $(dirname $(realpath $0)))"
+project_name="$(basename $project_path)"
+service_path="$(dirname $(realpath $0))"
+service_name="$(basename $service_path)"
+container_name="${project_name}_${service_name}_1"
+
 echo 'Updating configs...'
-docker cp nginx.conf docker-services_nginx_1:/etc/nginx/
-docker cp conf.d/default.conf docker-services_nginx_1:/etc/nginx/conf.d/
+docker cp "$service_path/nginx.conf" $container_name:/etc/nginx/
+docker cp "$service_path/conf.d/default.conf" $container_name:/etc/nginx/conf.d/
 echo 'Done.'
