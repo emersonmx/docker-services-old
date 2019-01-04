58ab48a44d4eb052dcc1f04d900dcd9b9e30afb8
Emerson MX <emersonmx@gmail.com>
2019-01-04 20:28:51 -0300

    Add script to run php-cs-fixer

---
+++ php-cs-fixer/docker-php-cs-fixer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_DIR=$SCRIPT_DIR
+
+docker run --rm \
+    --user $(id -u):$(id -g) \
+    --volume $PROJECT_DIR:/code \
+    php-cs-fixer $@
